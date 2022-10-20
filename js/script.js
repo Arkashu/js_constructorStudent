4df9d1462f72193e4da8189f7ec70d993538b7ad
Arkashu <kopeikaarkashu@gmail.com>
2022-10-20 13:55:27 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+function Student (name, lastName, birthYear) {
+    this.name = name;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.age = function () {
+        const today = new Date();
+        return today.getFullYear() - birthYear;
+    };
+    this.attendance = new Array(10).fill(null);
+    this.estimates = new Array(10).fill(null);
+    this.present = function () {
+        if (this.attendance.indexOf(null) === -1) return;
+        this.attendance[this.attendance.indexOf(null)] = true;
+    };
+    this.absent = function () {
+        if (this.attendance.indexOf(null) === -1) return;
+        this.attendance[this.attendance.indexOf(null)] = false;
+    };
+    this.mark = function (evaluation) {
+        if (this.estimates.indexOf(null) === -1) return;
+        this.estimates[this.estimates.indexOf(null)] = evaluation;
+    };
+    this.summary = function () {
+        const averageMark = this.estimates.reduce((a, b) => {
+            return (a + b)
+        }) / this.estimates.length;
+        const averagePresent = this.attendance.filter((element) => {
+            return element === true
+        }).length / this.attendance.length;
+        if (averageMark >= 9 && averagePresent >= 0.9) return 'Ути какой молодчинка!';
+        if (averageMark < 9 && averagePresent < 0.9) return 'Редиска!';
+        return 'Норм, но можно лучше';
+    }
+
+}
+const student_test = new Student('Vova', 'Cooper', 1990);
+console.log(student_test)
+student_test.present()
+student_test.present()
+student_test.present()
+student_test.present()
+student_test.present()
+student_test.present()
+student_test.present()
+student_test.absent()
+student_test.present()
+student_test.present()
+student_test.present()
+student_test.mark(9)
+student_test.mark(10)
+student_test.mark(10)
+student_test.mark(10)
+student_test.mark(6)
+student_test.mark(10)
+student_test.mark(8)
+student_test.mark(10)
+student_test.mark(10)
+student_test.mark(9)
+console.log(student_test.summary());
+
